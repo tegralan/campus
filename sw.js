e7d92667be6da13664c403f55e3271d4b2a37ec0
tegralan <144548520+tegralan@users.noreply.github.com>
2026-04-24 21:33:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'campus-bot-v2';
+const CACHE = 'campus-bot-v1';
 const ASSETS = ['./index.html', './manifest.json'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
